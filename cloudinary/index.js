ebc2028296e585bba1a722b8062cc2061b3ddfa8
TanveshVaity <tanveshvaity99@gmail.com>
2023-08-25 19:07:37 +0530

    added cloudinary api

---
+++ cloudinary/index.js
@@ -0,0 +1,23 @@
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "CampCraze",
+        allowedFormats: ["png", "jpeg", "jpg"],
+    }
+});
+
+
+
+module.exports = {
+    cloudinary,
+    storage,
+};
--- routes/campground.js
+++ routes/campground.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {isLoggedIn, campgroundValidation, isAuthor} = require("../middlleware");
 const campgrounds = require("../controllers/campground");
+const multer = require('multer');
+const {storage} = require("../cloudinary/index");
+const upload = multer({ storage });
 
 router.route("/")
-        .get(campgrounds.index)
-        .post(campgroundValidation, isLoggedIn, campgrounds.createNewCampground);
+    .get(campgrounds.index)
+    .post( isLoggedIn, upload.array("campground[image]"), campgroundValidation, campgrounds.createNewCampground);
 
 router.get("/new", isLoggedIn,campgrounds.newCampgroundForm);
 
